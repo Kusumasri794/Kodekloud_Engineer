b4fa83511e86c41c500df69c665ac39b63dc5ba5
kusuma794 <kusumasri794@gmail.com>
2023-06-27 19:47:12 +0530

    Kubernetes Sidecar Containers

--- Task-40.sh
+++ Task-40.sh
@@ -1,7 +1,5 @@
 # Install And Configure SFTP
-
 # exercise: Some of the developers from Nautilus project team have asked for SFTP access to at least one of the app server in Stratos DC. After going through the requirements, the system admins team has decided to configure the SFTP server on App Server 2 server in Stratos Datacenter. Please configure it as per the following instructions:
-
 # a. Create an SFTP user mariyam and set its password to TmPcZjtRQx.
 # b. Password authentication should be enabled for this user.
 # c. Set its ChrootDirectory to /var/www/app.
